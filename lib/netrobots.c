c0558b69a1e906900f31ab9f0ed687614ffbcae7
Jiri Benc <jbenc@upir.cz>
2013-07-19 00:29:15 +0200

    Updated README, clarified licensing
    Clarified licensing: my modifications to the client library have been under
    LGPL, by that the original library code fell under LGPL, too (the original
    client library was released as public domain).
    
    Added license headers to individual source files. Added copyright
    statements.

--- lib/netrobots.c
+++ lib/netrobots.c
@@ -1,3 +1,19 @@
+/*
+ * libnetrobots, client library for netrobots
+ * Copyright (C) 2008 Paolo Bonzini and others
+ * Copyright (C) 2013 Jiri Benc
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ */
+
 #include <string.h>
 #include <unistd.h>
 #include <netinet/in.h>
--- server/field.h
+++ server/field.h
@@ -1,8 +1,18 @@
-/* Map drawing module interface.
-   Paolo Bonzini, August 2008.
-
-   This source code is released for free distribution under the terms
-   of the GNU General Public License.  */
+/*
+ * netrobots, map drawing module interface
+ * Copyright (C) 2008 Paolo Bonzini and others
+ * Copyright (C) 2013 Jiri Benc
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
 
 #ifndef MAP_H
 #define MAP_H
